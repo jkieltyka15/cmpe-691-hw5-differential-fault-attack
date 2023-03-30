81745af072c3071918d3a4fbc7040b4af6a6f809
jkieltyka15 <jmkielty@mtu.edu>
2023-03-29 20:14:33 -0400

    refs #4 - Created prototypes for sbox functions and implemented sbox_get.

--- src/aes-sbox.c
+++ src/aes-sbox.c
@@ -1,5 +1,15 @@
 #include <stdint.h>
+#include <stdbool.h>
 
 #include "aes-sbox.h"
 
 uint8_t sbox[256];
+
+
+bool sbox_load(char* file_path) {
+    return false;
+}
+
+uint8_t sbox_get(uint8_t byte) {
+    return sbox[byte];
+}
